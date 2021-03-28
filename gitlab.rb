10e2f5173d1259e77d57b868b1bd358ee171575e
OlivierJM <manolivier93@gmail.com>
2021-03-28 21:55:10 +0200

    refactor

--- gitlab.rb
+++ gitlab.rb
@@ -4,16 +4,18 @@ require 'json'
 # get the env variables e.g: token
 # this needs to be set in the terminal like 
 token = ENV['GITLAB_TOKEN']
-project_id = "13905080"
-base_url = "https://gitlab.com/api/v4/projects/13905080"
+@base_url = "https://gitlab.com/api/v4/projects/13905080"
+@auth = {'Authorization' => "Bearer #{token}"}
 
 # fetch the last release in gitlab (tag)
 def last_tag
-    tags_url = URI("#{base_url}/repository/tags")
-    dep_response = Net::HTTP.get(tags_url, {'Authorization' => "Bearer #{token}"})
+    tags_url = URI("#{@base_url}/repository/tags")
+    dep_response = Net::HTTP.get(tags_url, @auth)
     convert = JSON.parse(dep_response)
     convert.last["name"]
 end
+
+puts last_tag
 
 
 
